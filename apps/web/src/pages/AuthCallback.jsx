3ca3a2df800ed2497157b4db7ebf5837232b74d0
Abdusamad Sherkulov <abdusamadsherkulov@gmail.com>
2026-07-19 17:00:09 +0500

    add auth callback page to store jwt token

---
+++ apps/web/src/pages/AuthCallback.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+function AuthCallback() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = searchParams.get('token');
+    if (token) {
+      localStorage.setItem('token', token);
+      navigate('/');
+    } else {
+      navigate('/login-failed');
+    }
+  }, [searchParams, navigate]);
+
+  return <p>Logging you in...</p>;
+}
+
+export default AuthCallback;
